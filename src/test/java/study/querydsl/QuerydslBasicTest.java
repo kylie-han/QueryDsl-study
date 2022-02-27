26dc8e57323b4d67698b9b84052a8b2463d134b7
Jihye Han <tomato960124@gmail.com>
2022-02-27 23:50:37 +0900

    동적쿼리 BooleanBuilder사용

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -110,6 +111,7 @@ public class QuerydslBasicTest {
         // get member single object
         Member fetchOne = queryFactory
                 .selectFrom(QMember.member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
         // get member first object
         Member fetchFirst = queryFactory
@@ -547,5 +549,26 @@ public class QuerydslBasicTest {
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
+    }
+    @Test
+    public void dynaminQuery_BooleanBuilder() {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 }
