3452ed75379f9a01618cde0d7dd2f86562b2a4f4
Jihye Han <tomato960124@gmail.com>
2022-02-27 22:48:56 +0900

    select문에 값을 나열하는, 그중 한 값만 조회하는 simpleProjection

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -441,7 +441,17 @@ public class QuerydslBasicTest {
                 .fetch();
         for (String s : result) {
             System.out.println("s = " + s);
-            
+        }
+    }
+
+    @Test
+    public void simpleProjection(){
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+        for (String s : result) {
+            System.out.println("s = " + s);
         }
     }
 }
